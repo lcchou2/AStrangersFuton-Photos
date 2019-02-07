ccf80a16b772330df1f1d74d8d129221be7d221b
Mehul Mandviya <mehul@Mehuls-MacBook-Pro.local>
2019-02-06 20:34:41 -0800

    Implement the basic connection

---
+++ similar-listing/server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3004
+
+app.use(express.static(path.join(__dirname, '../client')));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
