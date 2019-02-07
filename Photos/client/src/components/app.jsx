0616e19ce3fb840c3d8e6bb35e39002c170eda98
Lawrence Chou <lawrencechou@Lawrences-MacBook-Pro.local>
2019-02-06 20:06:52 -0800

    Set up working React Dom

---
+++ Photos/client/src/components/app.jsx
@@ -0,0 +1,18 @@
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+
+      <div>
+
+        Hello Photos
+      </div>
+    )
+  }
+}
+
+export default App
