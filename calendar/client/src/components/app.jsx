c2135c696906328261c976a2ca9c508f3eb3f9c0
Michael Hamm <mikejameshamm@gmail.com>
2019-02-06 20:20:04 -0800

    Setup cal env

---
+++ calendar/client/src/components/app.jsx
@@ -0,0 +1,13 @@
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <h1>Hello world!</h1>
+    );
+  }
+}
+
+export default App;
