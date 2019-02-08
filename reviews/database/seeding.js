07b22c4b8f913eba9ad038f7beba7e272e56bb69
Irene Shao <ireneshao@gmail.com>
2019-02-07 17:20:33 -0800

    Implement data.js and seeding.js to create data array of 100 listings

---
+++ reviews/database/seeding.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+var data = require('./data.js')
+
+
+function getRndInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+var Reviews = function(name, month, year, review) {
+  var obj = {
+    name: name,
+    profile: "http://i.pravatar.cc/100",
+    month: month,
+    year: year,
+    review: review
+  }
+  return obj;
+}
+
+var Listings = function(id) {
+  var ratings = [getRndInt(1, 5), getRndInt(1, 5), getRndInt(1, 5), getRndInt(1, 5), getRndInt(1, 5), getRndInt(1, 5)];
+  var totalRating = 0;
+  for (var i = 0; i < ratings.length; i++) {
+    totalRating += ratings[i];
+  }
+  var aveRating = totalRating / 6;
+
+  var reviews = [];
+  var index = getRndInt(1, 20);
+  for (var i = 0; i < index; i++) {
+    var name = data.name[getRndInt(0, 19)];
+    var month = data.month[getRndInt(0, 11)];
+    var year = data.year[getRndInt(0, 1)];
+    var review = data.review[getRndInt(0, 19)];
+
+    reviews.push(Reviews(name, month, year, review));
+  }
+
+  var obj = {
+    id: id,
+    rating: aveRating,
+    accuracy: ratings[0],
+    communication: ratings[1],
+    cleanliness: ratings[2],
+    location: ratings[3], 
+    checkin: ratings[4],
+    value: ratings[5],
+    reviews: reviews
+  }
+  return obj;
+}
+
+var dataArr = [];
+for (var i = 0; i < 100; i++) {
+  dataArr.push(Listings(i));
+}
+
+module.exports = {
+  Listings
+}
