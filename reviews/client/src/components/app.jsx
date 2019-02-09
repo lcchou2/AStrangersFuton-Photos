7037753397624d4b98e04e15a28cc1d796e3e290
Irene Shao <ireneshao@gmail.com>
2019-02-08 19:52:30 -0800

    Implement overall star rating

--- reviews/client/src/components/app.jsx
+++ reviews/client/src/components/app.jsx
@@ -1,11 +1,22 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      numOfReviews: 0
+    }
   }
 
   render() {
     return (
-      <div>Hello world!</div>
+    <div>
+      <div className="rating">
+        <span className='numOfReviews'>{this.state.numOfReviews} Reviews </span>
+        <span className="stars-container stars-90">★★★★★</span>
+      </div>
+      <hr/>
+
+    </div>
     );
   }
 }
