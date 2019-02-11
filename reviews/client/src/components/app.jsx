d9548209d2e39ac3e4be5f2c6d3aa76ce5d49218
Irene Shao <ireneshao@gmail.com>
2019-02-11 15:37:17 -0800

    Refactor database files so seeding.js would be run once using 'npm run seed'

--- reviews/client/src/components/app.jsx
+++ reviews/client/src/components/app.jsx
@@ -1,13 +1,33 @@
 import Search from './Search.jsx';
 import Order from './Order.jsx';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      listingData: null,
       numOfReviews: 0
     }
+    this.getListingData = this.getListingData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getListingData('1', (data) => {
+      this.setState({listingData: data[0]})
+      // console.log('data', this.state.listingData.listingId);
+    });
+  }
+
+  getListingData(listingId, successCB) {
+    $.ajax({
+      url: 'http://localhost:3003/api/reviews',
+      method: 'POST',
+      data: JSON.stringify({listingId: listingId}),
+      contentType: 'application/json',
+      success: successCB
+    });
   }
 
   render() {
@@ -54,10 +74,11 @@ class App extends React.Component {
           <br/>
           <div className="date">October 2018</div>
         </div>
-        <div className="flag">Flag</div>
+        {/* <div className="flag">Flag</div> */}
+        <img className="flag" src="http://download.seaicons.com/icons/icons8/ios7/512/Very-Basic-Flag-icon.png"/>
       </div>
       <br/>
-      <div className="review">Actual review... etc. This place is great.</div>
+      <div className="review">Actual review... This place is great... etc</div>
       <br/>
       <hr/>
     </div>
---
+++ reviews/database/index.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var reviewsSchema = require('./schema.js');
+mongoose.connect('mongodb://localhost/testReviews');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected');
+});
+
+
+var Reviews = mongoose.model('Reviews', reviewsSchema);
+
+var saveListings = (dataArr, callback) => {
+  Reviews.insertMany(dataArr, {ordered: false}, callback);
+}
+
+var returnListing = (listingId, callback) => {
+  Reviews.find({listingId: listingId}, '-_id -reviews._id -__v', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('data', data);
+      callback(null, data);
+    }
+  })
+}
+
+module.exports = {
+  saveListings,
+  returnListing
+};
