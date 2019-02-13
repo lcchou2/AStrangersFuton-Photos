95e13adcd3572ef4ec4822a70bf168605a5498e0
Michael Hamm <mikejameshamm@gmail.com>
2019-02-13 11:38:55 -0800

    Fix package.json/webconfig to work for testing

--- calendar/webpack.config.js
+++ calendar/webpack.config.js
@@ -8,15 +8,18 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module : {
-    loaders : [
+  mode: 'development',
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-       }
+        test: /\.jsx?$/,
+        exclude: /(node_modules | bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
       }
     ]
   }
