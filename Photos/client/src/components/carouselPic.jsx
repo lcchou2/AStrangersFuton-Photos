9545ee6602630f8d06b0ef421c544a324bdd3ead
Lawrence Chou <lawrencechou@Lawrences-MacBook-Pro.local>
2019-02-20 12:17:40 -0800

    style all in 1 file

--- Photos/client/src/components/carouselPic.jsx
+++ Photos/client/src/components/carouselPic.jsx
@@ -10,17 +10,20 @@ class Carouselpic extends React.Component {
 
   render() {
     return(
+      <div className = 'phomodalCon'> 
+        <div className = 'phoimage' >
 
-      <div className = 'phoimage' >
+          <img src = {this.props.list.list[this.props.currentIndex].url} style = {{width: '720.3',height: '482.59', marginLeft: 'auto', marginRight: 'auto'}} onClick = {this.props.clickFunction}></img>
 
-        <img src = {this.props.list.list[this.props.currentIndex].url} style = {{width: '720.3',height: '482.59', backgroundSize: 'cover', backgroundPosition: 'center'}} onClick = {this.props.clickFunction}></img>
-        
-        
-        <div className = 'phoslider'>
-          {this.props.currentIndex + 1}/{this.props.list.list.length}                    {this.props.list.list[this.props.currentIndex].description}
+
+          <div className = 'phoslider'>
+            {this.props.currentIndex + 1}/{this.props.list.list.length}                    {this.props.list.list[this.props.currentIndex].description}
+          </div>
+
         </div>
-    
+
       </div>
+      
 
     
     )
